23a354518562f7f7975b73daf1733ac268a3e4f5
Nicolas Pepe <Nicolasnahuel94@gmail.com>
2023-02-22 18:19:58 -0300

    #1 Algorithm - FizzBuzz

--- Algorithms/Playgrounds/1.playground/Contents.swift
+++ Algorithms/Playgrounds/1.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var numbers = [Int]()
+
+for i in 1...2000 {
+    numbers.append(i)
+}
+
+for num in numbers {
+    if num % 15 == 0 {
+        print("\(num) fizzbuzz")
+    } else if num % 5 == 0 {
+        print("\(num) buzz")
+    } else if num % 3 == 0 {
+        print("\(num) fizz")
+    }
+}
