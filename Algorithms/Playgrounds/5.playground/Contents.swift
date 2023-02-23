9a755d42ad3e45feb39c70f20c797bca5422d9b6
Nicolas Pepe <Nicolasnahuel94@gmail.com>
2023-02-23 12:00:38 -0300

    #5: - Reverse every other word

---
+++ Algorithms/Playgrounds/5.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var mySentence = "Hola soy Nicolas Pepe"
+
+func reverseWords(sentence: String) -> String{
+    let words = mySentence.components(separatedBy: " ")
+    var newSentence = ""
+    
+    for index in 0...words.count - 1 {
+        let word = words[index]
+        
+        if newSentence != "" {
+            newSentence += " "
+        }
+        
+        if index % 2 == 1 {
+            let reverseWord = String(word.reversed())
+            newSentence += reverseWord
+        } else {
+            newSentence += word.reversed()
+        }
+    }
+    return newSentence
+}
+
+print(reverseWords(sentence: mySentence))
