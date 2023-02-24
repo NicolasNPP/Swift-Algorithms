be886e5e9e439a0c251ace126e4c55450e05787c
Nicolas Pepe <Nicolasnahuel94@gmail.com>
2023-02-24 12:44:54 -0300

    #9 - Recursive Search through Binary Tree

---
+++ Algorithms/Playgrounds/9.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+class Node {
+    let value: Int
+    var leftChild: Node?
+    var rightChild: Node?
+    
+    init(value: Int, leftChild: Node?, rightChild: Node?) {
+        self.value = value
+        self.leftChild = leftChild
+        self.rightChild = rightChild
+    }
+}
+
+//Left branch
+let oneNode = Node(value: 1, leftChild: nil, rightChild: nil)
+let fiveNode = Node(value: 5, leftChild: oneNode, rightChild: nil)
+
+//Right branch
+let elevenNode = Node(value: 11, leftChild: nil, rightChild: nil)
+let twentyNode = Node(value: 20, leftChild: nil, rightChild: nil)
+let fourteenNode = Node(value: 14, leftChild: elevenNode, rightChild: twentyNode)
+
+let rootNode = Node(value: 10, leftChild: fiveNode, rightChild: fourteenNode)
+
+func search(node: Node?, searchValue: Int) -> Bool {
+    if node == nil {
+        return false
+    }
+    
+    if node?.value == searchValue {
+        return true
+    } else if searchValue < node!.value {
+        return search(node: node?.leftChild, searchValue: searchValue)
+    } else {
+        return search(node: node?.rightChild, searchValue: searchValue)
+    }
+    
+    
+}
+
+print(search(node: rootNode, searchValue: 5))
+print(search(node: rootNode, searchValue: 522))
+
+
+
